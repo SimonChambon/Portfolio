f1868d1b730bd5dfa11e68d9547fc454d3f5bdae
SimonChambon <Simon.CHAMBON@cf2000.onmicrosoft.com>
2021-10-18 12:09:22 +0200

    Création du menu nav et chargement du switch sur l'index

---
+++ import/menu.php
@@ -0,0 +1,11 @@
+<nav>
+    <ul>
+        <li><a href="./">Accueil</a></li>
+        <li><a href="./?p=cv">CV</a></li>
+        <li><a href="./?p=tuto">Tuto</a></li>
+        <li><a href="./?p=admin">Admin</a></li>
+        <li><a href="./?p=contact">Contact</a></li>
+        <li><a href="./?p=galerie">Galerie</a></li>
+        <li><a href="./?p=blog">Blog</a></li>
+    </ul>
+</nav>
---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+if(!isset($_GET['p'])){
+    include_once "import/accueil.php";
+}else{
+    switch($_GET['p']){
+        case "cv":  
+            include_once "import/cv.php";
+            break;
+        case "tuto":
+            include_once "import/tuto.php";
+            break;
+        case "admin":
+            include_once "import/admin.php";
+            break;
+        case "blog":
+            include_once "import/blog.php";
+            break;
+        case "contact":
+            include_once "import/contact.php";
+            break;
+        case "galerie":
+            include_once "import/galerie.php";
+            break;
+        default:    
+            include_once "import/accueil.php";
+    }
+}
